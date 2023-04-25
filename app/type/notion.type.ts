dee217795eaa7ff8cc6cb9b430aa60e144285700
Minki Cheon <minki@gazilab.co>
2023-04-25 11:30:41 +0900

    complete definition of notion block types

--- app/type/notion.type.ts
+++ app/type/notion.type.ts
@@ -20,7 +20,94 @@ export type NotionBlockBasicDoc = {
   };
   has_children: boolean;
   archived: boolean;
-  type: notionBlockNamesDoc;
+};
+
+type NotionRichTextDoc = {
+  rich_text: {
+    type: string;
+    text: {
+      content: string;
+      link: null;
+    };
+    annotations: {
+      bold: boolean;
+      italic: boolean;
+      strikethrough: boolean;
+      underline: boolean;
+      code: boolean;
+      color: colorType;
+    };
+    plain_text: string;
+    href: null;
+  }[];
+  color: colorType;
+};
+
+export type NotionTextTypedoc = NotionBlockBasicDoc & {
+  type:
+    | notionBlockNamesDoc.PARAGRAPH
+    | notionBlockNamesDoc.QUOTE
+    | notionBlockNamesDoc.BULLETED_LIST_ITEM
+    | notionBlockNamesDoc.NUMBERED_LIST_ITEM
+    | notionBlockNamesDoc.TOGGLE;
+  paragraph?: NotionRichTextDoc;
+  quote?: NotionRichTextDoc;
+  bulleted_list_item?: NotionRichTextDoc;
+  numbered_list_item?: NotionRichTextDoc;
+  toggle?: NotionRichTextDoc;
+};
+
+export type NotionHeadingDoc = NotionBlockBasicDoc & {
+  type:
+    | notionBlockNamesDoc.HEADING_1
+    | notionBlockNamesDoc.HEADING_2
+    | notionBlockNamesDoc.HEADING_3;
+  heading_1?: NotionRichTextDoc;
+  heading_2?: NotionRichTextDoc;
+  heading_3?: NotionRichTextDoc;
+  is_toggleable: boolean;
+};
+
+export type NotionTodoDoc = NotionBlockBasicDoc & {
+  type: notionBlockNamesDoc.TO_DO;
+  to_do: NotionRichTextDoc;
+  checked: boolean;
+};
+
+export type NotionCodeDoc = NotionBlockBasicDoc & {
+  type: notionBlockNamesDoc.CODE;
+  caption: {
+    type: string;
+    text: {
+      content: string;
+      link: null;
+    };
+    annotations: {
+      bold: boolean;
+      italic: boolean;
+      strikethrough: boolean;
+      underline: boolean;
+      code: boolean;
+      color: colorType;
+    };
+    plain_text: string;
+    href: null;
+  }[];
+  code: NotionRichTextDoc;
+  language: string;
+};
+
+export type NotionCalloutDoc = NotionBlockBasicDoc & {
+  type: notionBlockNamesDoc.CALLOUT;
+  callout: NotionRichTextDoc;
+  icon: {
+    type: string;
+    emoji: object;
+  };
+};
+
+export type NotionDividerDoc = NotionBlockBasicDoc & {
+  type: notionBlockNamesDoc.DIVIDER;
 };
 
 export type NotionBlockDoc = NotionBlockBasicDoc & {
