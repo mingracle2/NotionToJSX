ce02cfe33504f492090d92a44b446c17601a4903
Minki Cheon <minki@gazilab.co>
2023-05-22 15:43:57 +0900

    chore: fix address to vercel

---
+++ app/lib/getNotionPage.ts
@@ -0,0 +1,26 @@
+export const notionIntegrationToken =
+  "Bearer secret_6Fa7uOy0Rlygt7aD7WNUX4z0sPtHj7Has4Gsjk3pMsx";
+
+export const getNotionPage = async (targetId: string) => {
+  // Call API and receive response
+  const response = await (
+    await fetch(
+      "https://notion-to-jsx-git-minki-serversideprops-mingracle2.vercel.app/api/notion/get-page",
+      {
+        method: "POST",
+        body: JSON.stringify({ value: targetId }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+  ).json();
+
+  const pageName = response.results ? response.results[0].title.plain_text : "";
+  const pageIcon = response.icon ? response.icon.emoji : "";
+  const pageCover = response.cover
+    ? response.cover[response.cover.type].url
+    : "";
+  // return pageName[0].plain_text;
+  return [pageName, pageIcon, pageCover];
+};
